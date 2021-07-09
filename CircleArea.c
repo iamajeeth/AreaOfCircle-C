a12c01ffc2ec39a1aa36c031f19177b8a23ed1ea
Ajeeth Natarajan <43535972+iamajeeth@users.noreply.github.com>
2021-07-09 10:32:14 +0530

    Add files via upload

---
+++ CircleArea.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+	float radius, result;
+	
+	printf("Enter your Radius:\n");
+	scanf("%f", &radius);
+	
+	result = (22.0/7) * radius * radius;
+	
+	printf("\nArea of circle with Radius = %f is %f", radius, result);
+	
+	return 0;
+	
+	}
+
